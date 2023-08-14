ffea053d23d543e3bf6f7c6c32a267ffa1dbdf63
bazrafshanmh <bazrafshan.m.h@gmail.com>
2023-08-14 09:14:26 +0330

    Do Spawning process, but with bugs

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
     [SerializeField] public float degree = 3;
+    [ser]
 
     
     void Update()
     {
         transform.Rotate(new Vector3(0, 0, degree));
-
+        
     }
 }
