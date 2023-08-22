6c4c2e3981b27e313045aad86e9e1cb3a8804eb7
bazrafshanmh <bazrafshan.m.h@gmail.com>
2023-08-22 05:53:56 +0330

    Some Images adding

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -16,8 +16,8 @@ public class Spawner : MonoBehaviour
 
     void KnifeSpawner()
     {
-        GameObject instantiatedObject = Instantiate(knife, new Vector3(26.1637f, -22.7f, 0), Quaternion.Euler(new Vector3(-5.564f, 2.52617f, 52.292f)));
-        instantiatedObject.transform.localScale =  new Vector3(3.09306f, 2.52617f, 2.6f);
+        GameObject instantiatedObject = Instantiate(knife, new Vector3(26.1637f, -22.7f, 0), Quaternion.Euler(new Vector3(-5.564f, 2.52617f, 0)));
+        instantiatedObject.transform.localScale =  new Vector3(6.208699f, 3.229203f, 2.6f);
 
 
     }
